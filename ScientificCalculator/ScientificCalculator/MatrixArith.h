2edb310a2ac546d39a0b940cbc4289580af7e794
Nauman Shakir <naumanshakir3s@outlook.com>
2016-07-04 11:30:49 +0500

    working matrix addition

--- ScientificCalculator/ScientificCalculator/MatrixArith.h
+++ ScientificCalculator/ScientificCalculator/MatrixArith.h
@@ -8,14 +8,14 @@ private:
 	array<System::String^, 2>^ matrix1;
 	array<System::String^, 2>^ matrix2;
 	array<System::String^, 2>^ res;
-
+	
 
 };
 
 void MatrixCalculator::getMatrix(array<System::String^, 2>^ mat1, array<System::String^, 2>^ mat2) {
 	using namespace System;
 	//mat1->GetLength();
-
+	
 	matrix1 = gcnew array<String^, 2>(mat1->Rank,mat1->GetLength(0));
 	matrix2 = gcnew array<String^, 2>(mat2->Rank, mat2->GetLength(0));
 	
@@ -31,9 +31,9 @@ System::String^ MatrixCalculator::Add() {
 	for (int i = 0; i < matrix1->GetLength(0); i++) {
 		//StrRes = StrRes + "[";
 		for (int j = 0; j <matrix1->GetLength(0); j++) {
-			res[i,j] = matrix1[i, j] + matrix2[i, j];
+			res[i,j] =Convert::ToString(Convert::ToDouble( matrix1[i, j]) +Convert::ToDouble( matrix2[i, j]));
 			StrRes = StrRes + res[i, j]+",";
-			
+	
 		}
 		//StrRes = StrRes + "]";
 	}
